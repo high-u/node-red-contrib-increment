df103cdcf5e5c2f6da0ecb3380a22b9348ad3224
Wojtek Siudzinski <admin@suda.pl>
2017-11-21 12:42:19 +0100

    Add factor property

--- increment.js
+++ increment.js
@@ -1,22 +1,33 @@
 module.exports = function (RED) {
-    function increment(val) {
-      if (typeof val === 'undefined') return 1;
-      return Number(val) + 1;
+    function isEmpty(val) {
+      return typeof val === 'undefined'
+          || val === 'undefined'
+          || val === '';
+    }
+
+    function increment(val, factor) {
+      if (isEmpty(factor)) {
+          factor = 1;
+      }
+      factor = Number(factor);
+      if (isEmpty(val)) return factor;
+      return Number(val) + factor;
     }
     function incNode(config) {
         RED.nodes.createNode(this, config);
         this.kind = config.kind;
         this.target = config.target;
+        this.factor = config.factor;
         var node = this;
         this.on('input', function (msg) {
             if (!this.kind || this.kind === 'msg') {
-              msg[node.target] = increment(msg[node.target]);
+              msg[node.target] = increment(msg[node.target], node.factor);
               console.log(' msg[node.target]', msg[node.target], node)
 
             } else if (this.kind === 'flow') {
-              node.context().flow.set(node.target, increment(node.context().flow.get(node.target)));
+              node.context().flow.set(node.target, increment(node.context().flow.get(node.target), node.factor));
             } else if (this.kind === 'global') {
-              node.context().global.set(node.target, increment(node.context().global.get(node.target)));
+              node.context().global.set(node.target, increment(node.context().global.get(node.target), node.factor));
             }
             node.send(msg);
         });
